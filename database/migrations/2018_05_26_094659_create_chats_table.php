7de588c7bfd09ddf38925342554e24392cc93d24
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-05-26 12:50:11 +0300

    add schemas

---
+++ database/migrations/2018_05_26_094659_create_chats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('message_id');
+            $table->unsignedInteger('session_id');
+            $table->unsignedInteger('user_id');
+            $table->boolean('type'); // 0 is for send and 1 for receive
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
